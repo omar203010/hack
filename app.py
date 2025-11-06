5854e9c9d356e1f1eeeb783a2ac7705b34fdadf6
omar203010 <omar.m.alanziy@gmail.com>
2025-11-06 19:31:03 +0300

    first commit

--- app.py
+++ app.py
@@ -138,9 +138,6 @@ def api_images():
         return jsonify({'success': False, 'message': str(e)}), 500
 
 if __name__ == '__main__':
-    print("=" * 50)
-    print("تم تشغيل الخادم!")
-    print("افتح المتصفح وانتقل إلى: http://localhost:5000")
-    print("=" * 50)
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
 
